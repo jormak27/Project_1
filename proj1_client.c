982e84dfec7062e6187fa1fe270315e6773ed632
iamanogre <gqhoang@berkeley.edu>
2015-02-14 14:09:48 -0800

    Added overhead info

--- proj1_client.c
+++ proj1_client.c
@@ -1,6 +1,42 @@
-//import a lot of things
-#import <>
+/* EE122 Client Program */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+/* The client program is to connect to the specified server on the specified port, 
+receive a transferred file using the specified mode, and to save the given file to disk 
+at the specified location. When running the client, it should attempt to connect to 
+the server, retrieve the file, and exit. The client should also print out statistics 
+information of how long the connection lasted (in seconds) and how large the received 
+file is. In addition to the received file itself, the 
+client is required to export a second file listing the time-delay (in seconds) between each 
+packet received from the server. */
+
+/*
+	Usage: ./proj1_client <mode> <server_address> <port> <received_filename> <stats_filename>
+
+	<mode> Either 0, 1, or 2.  This value indicates what type of connection to establish with the server.  
+		It is assumed that the same mode is specified for both the server and the client.
+
+	<address> The IP address of the server to connect to.
+	
+	<port> The port number of the server to connect to.  
+		Use the same number here as was used to initialize the server.
+
+	<filename> Where to store the file on the local machine after it has been transferred from the server.
+
+	<stats_filename> Where to write the file containing statistics information 
+		about the received packets from the server.  This should be a text file, 
+		where each line indicates the delay between successive packets, as a floating-point number in seconds.
+
+	Example: This call open a client to connect to the server located at 128.32.47.83 
+		using datagram sockets (UDP) on port 33122, and will retrieve the file foo.txt 
+		and write out statistics info to stats.txt :
+	./proj1_client 1 128.32.47.83 33122 bar.txt stats.txt
+
+*/
 
 int main(int argc, char** argv) {
-	
+
 }
--- proj1_server.c
+++ proj1_server.c
@@ -1,7 +1,43 @@
-// import a lot of things
-#import <>
+/* EE122 Server Program */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+
+/* The server program is to transfer a specified file to any connecting clients.   
+It only needs to connect to one client at a time, but it should be able to 
+serve multiple clients in sequence without needing to be restarted. */
+/*
+	Usage: ./proj1_server <mode> <port> <filename> <packet_size> <packet_delay>
+	
+	<mode> Either 0, 1, or 2.  This value indicates what type of connection to establish with the server.
+		   0 :  Connection-oriented sockets 
+		   1 :  Connectionless sockets 
+		   2 :  Connectionless sockets without checksum enabled
+
+	<port> The port number to use for the server
+
+	<filename> A path to a local file to host.  
+			When a client connects to the server, it will be served this file 
+			using the protocol specified by <mode>.
+
+	<packet_size> The size of each packet to send, in bytes.  
+			You will need to break the input file into packets of this size, 
+			and send the packets separately through the network.
+
+	<packet_delay> The server should sleep for this many seconds between sending packets.  
+			Note that the value can be expressed as a floating point value.  
+			For sleeping in fractions of a second, look at the man page for usleep().
+
+	Example: The following call will specify the server to use datagram sockets (UDP) 
+			 on port 33122, hosting a file called bar.txt in a folder named foo.  
+			 The program will send the file in packets of size 1000 bytes, with zero delay between packets.
+		./proj1_server 1 33122 foo/bar.txt 1000 0
+*/
 
 int main(int argc, char** argv) {
-	
+	// recall command line arguments by doing argv[0]....argv[4]
+
+
 }
